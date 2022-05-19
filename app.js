eb12f5ad60e8404118cf57c2e1fd65bb0c16176e
dor achinakar <dor259dor@gmail.com>
2022-05-19 10:46:27 +0300

    order on app.js

--- app.js
+++ app.js
@@ -1,61 +1,74 @@
-const express = require("express"),
- cors = require('cors'),
-  app = express(),
+//express mongose
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = 2423;
+//cors
+const cors = require("cors");
+let corsOptions = {
+  origin: "http://example1.com",
+  optionsSuccessStatus: 200,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
 
-  mongoose = require("mongoose"),
-  chalk = require("chalk"),
-  // siteRouter = require("./routers/siterouter"),
-  // scrapedRouter = require("./routers/scrapedrouter");
-  port = 2423;
+//chalk
+const chalk = require("chalk");
+const log = chalk.bold.white.bgGreen;
+const error = chalk.bold.white.bgRed;
+
+// siteRouter = require("./routers/siterouter"),
+// scrapedRouter = require("./routers/scrapedrouter");
+
+//middelware
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
-
+app.use(cors());
 // app.use("/site", siteRouter);
-// app.use("/scraped", scrapedRouter);
+// app.use("/scraped", scrapedRouter)
+
+//morgan
 const morgan = require("morgan");
+app.use(morgan("tiny", { stream: accessLogStream }));
+
+//import that we build
 const User = require("./models/user");
 const { addUser, getAllUsers } = require("./controllers/users");
+
+//js import
 var fs = require("fs");
 var path = require("path");
 
-var corsOptions = {
-  origin: 'http://example1.com',
-  optionsSuccessStatus: 200 ,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-}
-
-const log = chalk.bold.white.bgGreen,
-error = chalk.bold.white.bgRed;
-
-var accessLogStream = fs.createWriteStream(path.join(__dirname, "logger.log"), {
+//logger
+let accessLogStream = fs.createWriteStream(path.join(__dirname, "logger.log"), {
   flags: "a",
 });
 
-app.use(morgan("tiny", { stream: accessLogStream }));
-
-app.get("/getALLUsers",cors(corsOptions) ,(req, res) => {
+//all get req
+app.get("/getALLUsers", cors(corsOptions), (req, res) => {
   getAllUsers()
     .then((user) => {
-    console.log(log("get all user"));
-    res.status(201).json("sucsses")
+      console.log(log("get all user"));
+      res.status(201).json("sucsses");
     })
-    .catch((err) => res.json(error(err)))
+    .catch((err) => res.json(error(err)));
 });
-
+//all post req
 app.post("/addUser", (req, res) => {
   let { name, password, phone, isVip } = req.body;
   const user = { name, password, phone, isVip };
   addUser(user)
-    .then((user) => {res.json((user))
-    console.log(log("user is addit"))
+    .then((user) => {
+      res.json(user);
+      console.log(log("user is addit"));
     })
-    .catch((err) => {res.json((err))
-    console.log(error(err))
-    })
+    .catch((err) => {
+      res.json(err);
+      console.log(error(err));
+    });
 });
 
+//connection
 mongoose
   .connect("mongodb://0.0.0.0:27017/project-node")
   .then(() => {
--- controllers/users.js
+++ controllers/users.js
@@ -1,30 +1,22 @@
 const User = require("../models/user");
 
+function addUser(user) {
+  return new Promise((resolve, reject) => {
+    const newUser = new User(user);
+    newUser
+      .save()
+      .then((user) => resolve(user))
+      .catch((err) => reject(err));
+  });
+}
 
-function addUser(user){
-    return new Promise ((resolve, reject)=>{
-
-        const newUser = new User( user );
-        newUser
-          .save()
-          .then((user) => resolve( user ))
-          .catch((err) => reject(err));
-    })}
-    function getAllUsers(){
-return new Promise((resolve, reject) => {
-    User
-.find()
-.then((user) => resolve(user))
-.catch((err) => reject(err));
-});
-};
-
-
+function getAllUsers() {
+  return new Promise((resolve, reject) => {
+    User.find()
+      .then((user) => resolve(user))
+      .catch((err) => reject(err));
+  });
+}
 
 exports.getAllUsers = getAllUsers;
-
-    
-
-
-
-exports.addUser = addUser
+exports.addUser = addUser;
