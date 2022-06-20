8f839a620ff661f39a77a8b7f2fd00352282649a
nimrodfaber <ngf1106@gmaile.com>
2022-06-20 19:33:56 +0300

    test 13 done bonus not yet

--- models/visitCard.js
+++ models/visitCard.js
@@ -1,30 +1,36 @@
 const { object } = require("joi");
 const mongoose = require("mongoose");
 const Joi = require("joi");
-const visitCardSchema = new mongoose.Schema({
-  businessName: {
-    type: String,
-    required: "The field name is a required field!",
-  },businessDiscribe: {
-    type: String,
-    required: "The field name is a required field!",
-  },businessAdress: {
-    type: String,
-    required: "The field name is a required field!",
-  },businessPhone: {
-    type: String,
-    required: "The field name is a required field!",
-  },businessPicture: {
-    type: String,
-    required: "The field name is a required field!",
-  },user_id: {
-    type: mongoose.Schema.Types.ObjectId, ref: 'User' ,
-  },likes:[Object]
-},
-{ timestamps: true });
-
-
-
+const visitCardSchema = new mongoose.Schema(
+  {
+    businessName: {
+      type: String,
+      required: "The field name is a required field!",
+    },
+    businessDiscribe: {
+      type: String,
+      required: "The field name is a required field!",
+    },
+    businessAdress: {
+      type: String,
+      required: "The field name is a required field!",
+    },
+    businessPhone: {
+      type: String,
+      required: "The field name is a required field!",
+    },
+    businessPicture: {
+      type: String,
+      required: "The field name is a required field!",
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    likes: [Object],
+  },
+  { timestamps: true }
+);
 
 visitCardSchema.methods.validateCardSchema = function (card) {
   const blogschema = Joi.object({
@@ -33,15 +39,9 @@ visitCardSchema.methods.validateCardSchema = function (card) {
     businessAdress: Joi.string().required(),
     businessPhone: Joi.string().required(),
     businessPicture: Joi.required(),
+    likes: Joi.required(),
   });
   return blogschema;
 };
 
-
-
-
-
-
 module.exports = mongoose.model("VisitCard", visitCardSchema, "visitCard");
-
-
